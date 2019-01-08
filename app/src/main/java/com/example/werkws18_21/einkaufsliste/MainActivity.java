0039f0ec420bd348b321909b61196ae5db77312b
werkws18-21 <werkws18-21@ifipool302-16.IFI-POOL>
2019-01-08 18:20:17 +0100

    initial commit

---
+++ app/src/main/java/com/example/werkws18_21/einkaufsliste/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.werkws18_21.einkaufsliste;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText name= findViewById(R.id.editText2);
+        EditText passwort= findViewById(R.id.editText);
+        Button buttonAcc = findViewById(R.id.button);
+        Button buttonP= findViewById(R.id.button2);
+
+        buttonAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this, Accounterstellen.class);
+                EditText editText = (EditText) findViewById(R.id.editText);
+                startActivity(intent);
+            }
+        });
+    }
+}
