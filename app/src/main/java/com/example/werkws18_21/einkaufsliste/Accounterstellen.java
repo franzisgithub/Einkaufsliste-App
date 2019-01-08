5e98b3f4bb8ef8766c27a5d26f06ddeb6c77fb8d
OGreulich <45317272+ogreulich@users.noreply.github.com>
2019-01-08 18:29:43 +0100

    test

--- app/src/main/java/com/example/werkws18_21/einkaufsliste/Accounterstellen.java
+++ app/src/main/java/com/example/werkws18_21/einkaufsliste/Accounterstellen.java
@@ -10,5 +10,7 @@ public class Accounterstellen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounterstellen);
         Intent intent = getIntent();
+
+
     }
 }
