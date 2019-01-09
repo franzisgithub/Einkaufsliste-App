c793b6659f5626e4e3af8260e83f53e16be4f1b8
OGreulich <45317272+ogreulich@users.noreply.github.com>
2019-01-09 10:10:45 +0100

    test3

---
+++ app/src/main/java/com/example/werkws18_21/einkaufsliste/GruppenManager.java
@@ -0,0 +1,13 @@
+package com.example.werkws18_21.einkaufsliste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GruppenManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gruppen_manager);
+    }
+}
